1685366a046d252d63eac7fd50d69147dd32d575
Riski Faozi <riskif2899@gmail.com>
2022-06-12 23:10:09 +0700

    create index.html style.css index.js

---
+++ index.js
@@ -0,0 +1,136 @@
+const data = {
+  currentNumber: "0",
+  calculatorOperator: "",
+  prevNumber: "",
+};
+
+let { currentNumber, calculatorOperator, prevNumber } = data;
+
+function updateDisplay() {
+  document.querySelector(".display__number").innerText = currentNumber;
+}
+
+function inputDigit(digit) {
+  if (currentNumber === "0") {
+    currentNumber = digit;
+  } else {
+    currentNumber += digit;
+  }
+}
+
+function clearCalculator() {
+  prevNumber = "";
+  calculatorOperator = "";
+  currentNumber = "0";
+}
+
+function inverseNumber() {
+  if (currentNumber === "0") return;
+
+  currentNumber = currentNumber * -1;
+}
+
+function inputOperator(operator) {
+  if (calculatorOperator === "") {
+    prevNumber = currentNumber;
+  }
+  calculatorOperator = operator;
+  currentNumber = "";
+}
+
+function inputDecimal(dot) {
+  if (currentNumber.includes(".")) return;
+
+  currentNumber += dot;
+}
+
+function performCalculator() {
+  let result = "";
+
+  switch (calculatorOperator) {
+    case "+":
+      result = prevNumber + currentNumber;
+      break;
+    case "-":
+      result = prevNumber - currentNumber;
+      break;
+    case "×":
+      result = prevNumber * currentNumber;
+      break;
+    case "÷":
+      result = prevNumber / currentNumber;
+      break;
+
+    default:
+      break;
+  }
+
+  currentNumber = result;
+  calculatorOperator = "";
+}
+
+function calculate() {
+  let result = "";
+  let divide = null;
+
+  switch (calculatorOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = parseFloat(prevNumber) - parseFloat(currentNumber);
+      break;
+    case "×":
+      result = parseFloat(prevNumber) * parseFloat(currentNumber);
+      break;
+    case "÷":
+      divide = parseFloat(prevNumber) / parseFloat(currentNumber);
+      result = divide.toFixed(2);
+      break;
+
+    default:
+      break;
+  }
+
+  currentNumber = result;
+}
+
+const buttons = document.querySelectorAll(".calculator__key");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.classList.contains("key--allclear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("key--plusmins")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("key--equalsign")) {
+      calculate();
+      updateDisplay(currentNumber);
+      return;
+    }
+
+    if (target.classList.contains("key--operator")) {
+      inputOperator(target.innerText);
+      return;
+    }
+
+    if (target.classList.contains("key--decimal")) {
+      inputDecimal(target.innerText);
+      updateDisplay(currentNumber);
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+});
